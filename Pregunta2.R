38bacd736f6cb7f29210056c7525d0e256f6e7d5
fangxin.chen <fangxin.chen@auleseebe.upc.edu>
2024-10-24 11:32:47 +0200

    expppppp

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,9 @@
-
+lambda <- 1/10000#E(t)=1/lambda=10000 -> lambda = 1/10000
+1-pexp(13000,lambda)
+qexp(0.8,lambda)
+set.seed(85)
+simulacio<-rexp(100,lambda)
+mean(simulacio)
+median(simulacio)
+var(simulacio)
+#exponencial: dexp-> f, pexp-> F, qexp -> F^-1, rexp->
